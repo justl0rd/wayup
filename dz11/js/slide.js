c7e616949799b5a895654c23bcf062cf453a4067
Justl0rd <marmorela@ukr.net>
2018-12-09 18:19:06 +0200

    services page add

---
+++ dz11/js/slide.js
@@ -0,0 +1,10 @@
+$('.reviews__slides').slick({
+			dots: true,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			pauseOnHover: true,
+			responsive: true,
+            appendArrows: $('.your-class-arrow'),
+            prevArrow: '<button class="prev slider-arrow-style" type="button" class="btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+            nextArrow: '<button class="next slider-arrow-style" type="button" class="btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+    	});
