d3854496d71d4d326cb8adc2fe279715d65e4861
Justl0rd <marmorela@ukr.net>
2018-12-11 20:57:11 +0200

    preloader add

--- dz11/js/callback.js
+++ dz11/js/callback.js
@@ -1,12 +1,18 @@
-$('.callback__open').click(function(){
-	$('.callback').css({'top': $(window).scrollTop() +50}).addClass('callback-active');
-	$('.callback-block').addClass('callback-active');
-	$('.callback-block__bg').click(function(){
-		$('.callback-block').removeClass('callback-active');
+$(function () {
+	/*Modal window*/
+	$('.callback__open').click(function(){
+		$('.callback').addClass('callback-active');
+		$('.callback-block').addClass('callback-active');
+		$('.callback-block__bg').click(function(){
+			$('.callback-block').removeClass('callback-active');
+		});
+	});
+
+	/*Scroll To*/
+	$("a.scroll-to").click(function (e) {
+		e.preventDefault();
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 800);
 	});
 });
-
-$(window).scroll(function() {
-	$('.callback').css({'top': $(window).scrollTop() +50});
-}).scroll();
-
